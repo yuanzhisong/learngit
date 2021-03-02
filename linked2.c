488d0e97313c4096d8aac622064487b255928471
yuanzhisong <songyuanzhi_ym@163.com>
2021-03-02 11:01:37 +0800

    linked2.c

--- linked2.c
+++ linked2.c
@@ -22,44 +22,51 @@ void find(struct node *head,int x)
 		printf("%d在链表的第%d个结点中！\n",x,i);
 }
 //插入新节点函数
-void ins(struct node *head,int j,int s)
+void ins(struct node *head,struct node *p,int j,int s)
 {
 	int a=1;	
 	struct node *new,*this;
 	new=malloc(sizeof(struct node));
 	this=head;
-	while((this!=NULL)&&!(this->d==j)){
+	while(this!=NULL&&!(this->d==j)){
 		this=this->next;
 		a++;
 	}
-	new->d=s;
-	new->next=this->next;
-	this->next=new;
+	if(j!=10){
+		new->d=s;
+		new->next=p->next;
+		p->next=new;
+	}
+	else{
+		new->d=s;
+		new->next=this->next;
+		this->next=new;
+	}
 }
 //删除节点
 void del(struct node *head,int b)
 {
 	struct node *new,*this;
 	this=head;
-	while(this->next!=NULL){
-		if(this->d==b){
-			this=head->next;
-			free(head);
-			head=this;
-		}
-		else{
+	if(this->d==b){
+		this=head->next;
+		free(head);
+		head=this;
+	}
+	else{
+		while(this->next!=NULL){
 			new=this->next;
 			if(new->d==b){
 				this->next=new->next;
 				free(new);
 			}
-                       else
-			       this=this->next;
-		}				
+			else		       
+				this=this->next;
+		}
 	}
 }
-void print()
-{
+	void print()
+	{
 	for(q=head;q!=NULL;q=q->next)
 		printf("%d",q->d);
 	printf("\n");
@@ -92,7 +99,7 @@ int main()
 		case 2:	
 			printf("在节点后增加的数字：");
 			scanf("%d %d",&j,&s);
-			ins(head,j,s);
+			ins(head,p,j,s);
 			print();
 			break;
 		case 3:
